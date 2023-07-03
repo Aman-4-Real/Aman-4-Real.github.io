882af0e914bd53dd3c10eecf080ff47898ad84ce
Aman-4-Real <cq335955781@gmail.com>
2023-07-03 11:31:16 +0800

    20230703: updates

--- bin/dockerhub_run.sh
+++ bin/dockerhub_run.sh
@@ -5,3 +5,5 @@ fi
 docker run --rm -v "$PWD:/srv/jekyll/" -p "8080:8080" \
                     -it amirpourmand/al-folio bundler  \
                     exec gem install jekyll; jekyll serve --watch --port=8099 --host=0.0.0.0 --trace
+
+# bundler exec jekyll clean
